7ac38c4b5de9790ad09d272b8f7f3977f3272c78
Divinka <divinka2009@gmail.com>
2025-09-27 16:32:26 +0700

    udah

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-    import { defineConfig } from 'vite';
-    import laravel from 'laravel-vite-plugin';
-    import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
-    export default defineConfig({
+export default defineConfig({
     plugins: [
         laravel({
-        input: [
-            'resources/css/app.css',
-            'resources/js/app.jsx', // pastikan sesuai dengan file yang kamu buat
-        ],
-        refresh: true,
+            input: ['resources/js/app.jsx', 'resources/css/app.css'],
+            refresh: true,
         }),
         react(),
     ],
-    });
+});
